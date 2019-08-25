d0bbb67f4cb40d9403463f0743eb2b620a6dad12
tamersalama <tamer.salama@noriatechnologies.com>
2019-08-25 22:12:50 +0000

    training but still loss is high

--- nbs/dl1/lesson3-planet.py
+++ nbs/dl1/lesson3-planet.py
@@ -21,7 +21,15 @@ planet_path = untar_data(URLs.PLANET_TINY)
 
 
 
-data = ImageList.from_csv(planet_path, 'labels.csv', folder='train', suffix='.jpg').split_by_rand_pct().label_from_df(label_delim = ' ').databunch()
+data = ImageList.from_csv(planet_path, 'labels.csv', folder='train', suffix='.jpg').split_by_rand_pct(0.2).label_from_df(label_delim = ' ')
+
+tfms = get_transforms(flip_vert=True, max_lighting=0.1, max_zoom=1.05, max_warp=0.)
+
+
+data = (data.transform(tfms, size=128)
+        .databunch().normalize(imagenet_stats))
+
+
 data.show_batch(rows=4, figsize=(10, 7))
 
 arch = models.resnet50
@@ -35,6 +43,10 @@ learn.lr_find()
 
 
 learn.recorder.plot()
+
+lr = 1e-01
+
+learn.fit_one_cycle(10, slice(lr))
 
 #
 # train_files = (planet_path / 'train-jpg').ls()
